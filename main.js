5436f8ca4376de4b3ed9c94787a1857a870451d8
Yes <Deeznutssoftware@skiff.com>
2022-06-01 19:10:35 +0200

    commit

---
+++ main.js
@@ -0,0 +1,15 @@
+function validateForm() {
+    var input = document.forms["myForm"]["cmd"].value;
+    if (input == "") {
+      alert("\n");
+      return false;
+    } else {
+      event.preventDefault();
+      const tag = document.createElement("p");
+      const text = document.createTextNode(input); 
+      tag.appendChild(text);
+      var element = document.getElementsByTagName("body")[0];
+      element.appendChild(tag);
+      document.querySelector('input').value=''
+    }
+  }
