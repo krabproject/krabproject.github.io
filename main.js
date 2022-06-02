cca8344c79333d8456dc5c601efbef2d3ca4f0b3
kommand-shell <106449123+kommand-shell@users.noreply.github.com>
2022-06-02 05:16:58 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,15 @@
+function validateForm() {
+    var input = document.forms["myForm"]["cmd"].value;
+    if (input == "") {
+      alert("no");
+      return false;
+    } else {
+      const tag = document.createElement("p");
+      const text = document.createTextNode(input); 
+      tag.appendChild(text);
+      var element = document.getElementsByTagName("body")[0];
+      element.appendChild(tag);
+      document.querySelector('input').value=''
+      event.preventDefault();
+    }
+  }
