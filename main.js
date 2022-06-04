2546f43766aa1830766345d3c19917a2f9658780
boredcoder411 <taamarcos@gmail.com>
2022-06-04 14:40:32 +0200

    delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-function changeText2() {
-  const list = document.getElementById('demo');
-  const shell = "root@krabcli $  "
-  const firstname =   document.getElementById('firstname').value;
-  const entry = document.createElement('li');
-  list.appendChild(entry);
-  entry.appendChild(document.createTextNode(shell + firstname));
-  document.getElementById('firstname').value = ''
-}
